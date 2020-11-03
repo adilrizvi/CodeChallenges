7029c60b7aab53ea170fa9fd4e111087b96d80f9
Adil <adil9791@gmail.com>
2020-11-03 09:32:41 +0530

    stack implementation

---
+++ stack.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+)
+
+var emptyStackErr error = errors.New("empty stack")
+
+type stack struct {
+	top int
+	data []int
+}
+
+func (st *stack) push(val int) {
+	st.top += 1
+	st.data = append(st.data, val)
+}
+
+func (st *stack) topElement() (int, error){
+	if st.isEmpty() {
+		return -1, emptyStackErr
+	}
+	return st.data[st.top], nil
+
+}
+
+func (st *stack) pop() (int, error) {
+	if st.isEmpty() {
+		return -1, emptyStackErr
+	}
+	data := st.data[st.top]
+	st.top -= 1
+	return data, nil
+}
+
+func (st *stack) isEmpty() bool {
+	return st.top == -1
+}
+
+func (st *stack) size() int {
+	return st.top+1
+}
